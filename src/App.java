d8737a8c83e71549c9524cb8f92f6f7b95e38dd1
Elixer <everett.rumery@gmail.com>
2025-04-24 13:30:15 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setSize(1000,800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("oOOooOOO AaaaAAaaAA");
+        frame.add(new Mondy(frame.getWidth(), frame.getHeight(), 10));
+        frame.setVisible(true);
+
+        frame.repaint();
+
+    }
+}
---
+++ src/Mondy.java
@@ -0,0 +1,48 @@
+﻿import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Mondy extends JPanel {
+    private int width;
+    private int height;
+    private int depth;
+
+    public static Color[] colors = {Color.black, Color.red, Color.white, Color.blue, Color.yellow};
+
+    public Mondy(int w, int h, int d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        mondy(g,0,0,width,height,depth);
+
+
+
+    }
+
+    public void mondy(Graphics g, int x, int y, int w, int h, int d) {
+        if (d <= 0) {
+            return;
+        }
+
+        g.setColor(colors[(int)(Math.random()*colors.length)]);
+        g.fillRect(x, y, w, h);
+        g.setColor(Color.gray);
+        g.drawRect(x, y, w, h);
+
+        if(Math.random()<0.5) { //a 50/50 coin flip HORIZONTAL
+            mondy(g, x, y, w, h/2, d-1);
+            mondy(g, x, y + h/2, w, h/2, d-1);
+        }
+        else {
+            mondy(g, x, y, w/2, h, d-1);
+            mondy(g, x + w/2, y, w/2, h, d-1);
+        }
+
+    }
+}
